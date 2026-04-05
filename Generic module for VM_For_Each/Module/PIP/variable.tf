afd1074cb66680d8213f3805d477953dd54c528b
snldby <snl.dby@gmail.com>
2026-04-05 20:02:45 +0530

    Delete Generic module for VM_For_Each directory

--- Generic module for VM_For_Each/Module/PIP/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "public_ips" {
-  type = map(object({
-    pip_name = string
-    rg_name  = string
-    location = string
-  }))
-}
--- Generic module for VM_For_Each/Module/RG/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-for_each = var.rg_name
-  name     = each.value.rg_name
-  location = each.value.location
-  managed_by = each.value.managed_by
-  tags = each.value.tags
-
-}
-
